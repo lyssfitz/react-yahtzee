2b91ad2d6e0f64a3d9c8fcfc854afb067e611945
Lyss <lyssfitz@outlook.com>
2020-03-13 17:58:55 +1100

    Fix rolling text

--- src/Game.js
+++ src/Game.js
@@ -76,6 +76,11 @@ class Game extends Component {
 		this.animateRoll();
 	}
 
+	displayRollInfo() {
+		const messages = ['0 Rolls Left', '1 Roll Left', '2 Rolls Left', 'Starting Round'];
+		return messages[this.state.rollsLeft];
+	}
+
 	render() {
 		return (
 			<div className="Game">
@@ -96,7 +101,7 @@ class Game extends Component {
 								disabled={this.state.locked.every(x => x) || this.state.rollsLeft === 0}
 								onClick={this.animateRoll}
 							>
-								{this.state.rollsLeft} Rerolls Left
+								{this.displayRollInfo()}
 							</button>
 						</div>
 					</section>
